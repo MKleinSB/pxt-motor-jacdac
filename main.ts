b9f6b58bd9f4e2f59bd95ba5685be9103ca549d2
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2023-10-25 22:47:39 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace modules {
     /**
      * Calliope mini motor M0
      */
-    //% fixedInstance whenUsed block="makerbit motor M0"
+    //% fixedInstance whenUsed block="calliope motor M0"
     export const calliopeMotorM0 = new MotorClient("calliope motor M0?dev=self&srvo=0&name=M0")
     /**
      * Calliope mini motor M1
@@ -26,8 +26,8 @@ namespace servers {
     }
 
     function start() {
-        jacdac.productIdentifier = 0x3cadc101
-        jacdac.deviceDescription = "MakerBit Motor"
+        jacdac.productIdentifier = 0x3cad0000
+        jacdac.deviceDescription = "Calliope mini Motor"
         jacdac.startSelfServers(() => [
             jacdac.createActuatorServer(
                 jacdac.SRV_MOTOR,
