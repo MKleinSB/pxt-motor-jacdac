ee91ddc4b43359ecfe8e2d2e5cc007a1c51a8a10
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2024-03-28 21:05:38 +0100

    remove main

--- main.ts
+++
@@ -1,8 +0,0 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    modules.calliopeMotorM0.run(50)
-    modules.calliopeMotorM1.run(100)
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    modules.calliopeMotorM0.run(100)
-    modules.calliopeMotorM1.run(-100)
-})
