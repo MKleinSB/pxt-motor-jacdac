0ea3d17424b02c94a3a2fe8ea83b5f0941869d7a
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2024-03-28 16:07:48 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, jacdacmotors.ts

---
+++ jacdacmotors.ts
@@ -0,0 +1,46 @@
+
+namespace modules {
+    /**
+     * Calliope mini motor M0
+     */
+    //% fixedInstance whenUsed block="calliope motor M0"
+    export const calliopeMotorM0 = new MotorClient("calliope motor M0?dev=self&srvo=0&name=M0")
+    /**
+     * Calliope mini motor M1
+     */
+    //% fixedInstance whenUsed block="calliope motor M1"
+    export const calliopeMotorM1 = new MotorClient("calliope motor M1?dev=self&srvo=1&name=M1")
+}
+namespace servers {
+    function sync(server: jacdac.Server, motor: Motor) {
+        const speed = server.value
+        const enabled = !!server.intensity
+        if (speed === 0 || isNaN(speed) || !enabled) {
+            motors.dualMotorPower(motor, 0)
+        } else {
+            motors.dualMotorPower(motor, Math.abs(speed) * 100)
+        }
+    }
+
+    function start() {
+        jacdac.productIdentifier = 0x3347a2d2 // Calliope mini
+        jacdac.deviceDescription = "Calliope mini Motor"
+        jacdac.startSelfServers(() => [
+            jacdac.createActuatorServer(
+                jacdac.SRV_MOTOR,
+                (server) => sync(server, Motor.M0), {
+                instanceName: "M0",
+                valuePackFormat: jacdac.MotorRegPack.Speed,
+                intensityPackFormat: jacdac.MotorRegPack.Enabled,
+            }),
+            jacdac.createActuatorServer(
+                jacdac.SRV_MOTOR,
+                (server) => sync(server, Motor.M1), {
+                instanceName: "M1", 
+                valuePackFormat: jacdac.MotorRegPack.Speed,
+                intensityPackFormat: jacdac.MotorRegPack.Enabled,
+            }),
+        ])
+    }
+    start()
+}
--- main.ts
+++ main.ts
@@ -1,48 +1,10 @@
-//% deprecated
-namespace motors { }
-
-namespace modules {
-    /**
-     * Calliope mini motor M0
-     */
-    //% fixedInstance whenUsed block="calliope motor M0"
-    export const calliopeMotorM0 = new MotorClient("calliope motor M0?dev=self&srvo=0&name=M0")
-    /**
-     * Calliope mini motor M1
-     */
-    //% fixedInstance whenUsed block="calliope motor M1"
-    export const calliopeMotorM1 = new MotorClient("calliope motor M1?dev=self&srvo=1&name=M1")
-}
-
-namespace servers {
-    function sync(server: jacdac.Server, motor: Motor) {
-        const speed = server.value
-        const enabled = !!server.intensity
-        if (speed === 0 || isNaN(speed) || !enabled) {
-            motors.brakeMotor(motor)
-            } else {
-            motors.dualMotorPower(motor, Math.abs(speed) * 100)
-        }
-    }
-
-    function start() {
-        jacdac.productIdentifier = 0x3cad0000
-        jacdac.deviceDescription = "Calliope mini Motor"
-        jacdac.startSelfServers(() => [
-            jacdac.createActuatorServer(
-                jacdac.SRV_MOTOR,
-                (server) => sync(server, Motor.A), {
-                instanceName: "M0",
-                valuePackFormat: jacdac.MotorRegPack.Speed,
-                intensityPackFormat: jacdac.MotorRegPack.Enabled,
-            }),
-            jacdac.createActuatorServer(
-                jacdac.SRV_MOTOR,
-                (server) => sync(server, Motor.B), {
-                instanceName: "M0", valuePackFormat: jacdac.MotorRegPack.Speed,
-                intensityPackFormat: jacdac.MotorRegPack.Enabled,
-            }),
-        ])
-    }
-    start()
-}
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    modules.calliopeMotorM0.run(50)
+    modules.calliopeMotorM1.run(100)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    modules.calliopeMotorM0.run(100)
+    modules.calliopeMotorM1.run(100)
+})
+modules.calliopeMotorM0.setEnabled(true)
+modules.calliopeMotorM1.setEnabled(true)
